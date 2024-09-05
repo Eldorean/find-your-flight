c1a15ec742dedd184e7c5c866917a768cf7f3584
Silvester Wennekers <silvester@familie-wennekers.nl>
2024-09-05 14:24:30 +0200

    feature/fetchFlightData

---
+++ src/utils/fetchFlightData/FlightInfo.types.ts
@@ -0,0 +1,10 @@
+export interface FlightInfoType {
+  flightIdentifier: string;
+  flightNumber: string;
+  airport: string;
+  date: string;
+  expectedTime: string;
+  originalTime: string;
+  url: string;
+  score: string;
+}
