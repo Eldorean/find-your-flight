a1044924c084d5e5837330540503be9648ee4ce1
Silvester Wennekers <silvester@familie-wennekers.nl>
2024-09-05 13:54:04 +0200

    update test config and first test

---
+++ src/components/input/Input.test.tsx
@@ -0,0 +1,50 @@
+import '@testing-library/jest-dom';
+import { render, fireEvent } from '@testing-library/react';
+import TextInput from './Input';
+import { vi } from 'vitest';
+
+const onChange = vi.fn();
+
+describe('Input', () => {
+  beforeEach(() => {
+    onChange.mockReset();
+  });
+
+  it('renders input element with correct value', () => {
+    const value = 'Hello';
+    const { getByRole } = render(<TextInput value={value} onChange={onChange} />);
+    const inputElement = getByRole('textbox') as HTMLInputElement;
+    expect(inputElement.value).toBe(value);
+  });
+
+  it('calls onChange callback when input value changes', () => {
+    const value = 'Hello';
+    const { getByRole } = render(<TextInput value={value} onChange={onChange} />);
+    const inputElement = getByRole('textbox') as HTMLInputElement;
+
+    const newValue = 'World';
+    fireEvent.change(inputElement, { target: { value: newValue } });
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toHaveBeenCalledWith('World');
+  });
+
+  it('updates input value when value prop changes', () => {
+    const { getByRole } = render(<TextInput value="Hello" onChange={onChange} />);
+    const inputElement = getByRole('textbox') as HTMLInputElement;
+
+    const newValue = 'World';
+    fireEvent.change(inputElement, { target: { value: newValue } });
+
+    expect(inputElement.value).toBe('World');
+  });
+
+  
+  it('renders input element with correct placeholder', () => {
+    const placeholder = 'Enter text';
+    const { getByPlaceholderText } = render(<TextInput placeholder={placeholder} value="" onChange={onChange} />);
+    const inputElement = getByPlaceholderText(placeholder) as HTMLInputElement;
+
+    expect(inputElement).toBeInTheDocument();
+  });
+});
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+type TextInputProps = {
+  placeholder?: string;
+  value: string;
+  onChange: (e: string) => void;
+};
+
+const TextInput = ({ placeholder, value, onChange }: TextInputProps) => {
+  const [inputValue, setInputValue] = useState(value);
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value;
+    setInputValue(text);
+    onChange(text);
+  }
+
+  return (
+    <input
+      className="input"
+      placeholder={placeholder}
+      value={inputValue}
+      onChange={onChangeHandler}
+    />
+  );
+};
+
+export default TextInput;
